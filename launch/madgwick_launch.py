732995cf425807b3b5e4c2fcc26d2a928b02b955
spereira02 <spereira@ethz.ch>
2026-03-09 17:54:04 +0100

    first commit

---
+++ launch/madgwick_launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+   
+    config = os.path.join(get_package_share_directory('imu_madgwick'),'config','imu_filter.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='imu_filter_madgwick',
+            executable='imu_filter_madgwick_node',
+            name='imu_filter_node',
+            output='screen',
+            parameters=[config],
+            
+        ),
+        Node(
+            package='micro_ros_agent',
+            executable='micro_ros_agent',
+            name='micro_ros_agent',
+            output='screen',
+            arguments=['serial', '--dev', '/dev/ttyUSB0']
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+        )
+    ])
